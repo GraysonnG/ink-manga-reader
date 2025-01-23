666b1aa67b2384b0ea405f18ba953ac9cd9bd137
Grayson Gullion <xinflux@gmail.com>
2025-01-22 19:32:59 -0500

    Feature: Create simple feature set for BaseViewModel

--- app/src/main/java/com/blanktheevil/inkmangareader/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/blanktheevil/inkmangareader/viewmodels/BaseViewModel.kt
@@ -1,16 +1,34 @@
 package com.blanktheevil.inkmangareader.viewmodels
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-abstract class BaseViewModel<T>(
+abstract class BaseViewModel<T : BaseViewModelState, R>(
     state: T,
 ) : ViewModel() {
     protected val _uiState = MutableStateFlow(state)
     val uiState = _uiState.asStateFlow()
 
+    abstract fun initViewModel(hardRefresh: Boolean, params: R? = null): Job
+
+    protected suspend fun doAsyncJobs(vararg jobs: suspend () -> Any) = coroutineScope {
+        val deferred = jobs.map { this.async { it() } }
+        deferred.awaitAll()
+        Unit
+    }
+
+
     fun updateState(update: T.() -> T) {
         _uiState.value = update(_uiState.value)
     }
+}
+
+abstract class BaseViewModelState {
+    abstract val errors: List<Any>
+    abstract val loading: Boolean
 }
