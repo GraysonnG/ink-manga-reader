f9caf4ff1876e01301da89dd1da43462833e5bc1
Grayson Gullion <xinflux@gmail.com>
2025-01-30 18:52:33 -0500

    Feature: Add ability to show placeholder images

--- app/src/main/java/com/blanktheevil/inkmangareader/ui/UIUtils.kt
+++ app/src/main/java/com/blanktheevil/inkmangareader/ui/UIUtils.kt
@@ -9,13 +9,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Scale
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+
+@Composable
+fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.toPx() }
+fun Dp.dpToPx(density: Density) = with(density) { this@dpToPx.toPx() }
+
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
+fun Int.pxToDp(density: Density) = with(density) { this@pxToDp.toDp() }
 
 @Composable
 fun InkIcon(
@@ -35,11 +47,13 @@ fun InkIcon(
 @Composable
 fun String?.toAsyncPainterImage(
     crossfade: Boolean = false,
+    @DrawableRes placeholder: Int? = null,
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ): AsyncImagePainter {
     val context = LocalContext.current
     return rememberAsyncImagePainter(
         model = ImageRequest.Builder(context)
+            .apply { placeholder?.let { placeholder(placeholder) } }
             .dispatcher(dispatcher)
             .data(this)
             .crossfade(crossfade)
