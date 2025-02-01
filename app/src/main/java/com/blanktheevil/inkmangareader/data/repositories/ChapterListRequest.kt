bc7acd85fd86e7d60b0ede963711e9627a94c49e
Grayson Gullion <xinflux@gmail.com>
2025-02-01 17:42:39 -0500

    Fix: ChapterListRequest.Feed not having a unique key

--- app/src/main/java/com/blanktheevil/inkmangareader/data/repositories/ChapterListRequest.kt
+++ app/src/main/java/com/blanktheevil/inkmangareader/data/repositories/ChapterListRequest.kt
@@ -4,6 +4,6 @@ sealed class ChapterListRequest(
     val type: String,
 ) {
     data class Generic(val ids: List<String>) : ChapterListRequest(type = ids.joinToString { it.takeLast(4) })
-    data class Feed(val mangaId: String) : ChapterListRequest(type = "Feed")
+    data class Feed(val mangaId: String) : ChapterListRequest(type = "Feed-$mangaId")
     data object Follows : ChapterListRequest(type = "Follows")
 }
