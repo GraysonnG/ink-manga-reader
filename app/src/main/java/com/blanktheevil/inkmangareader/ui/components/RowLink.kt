10a82397b4d230240f7b7c326c689e155211c009
Grayson Gullion <xinflux@gmail.com>
2025-02-08 01:29:50 -0500

    Feature: Allow RowLink to be clicked

--- app/src/main/java/com/blanktheevil/inkmangareader/ui/components/RowLink.kt
+++ app/src/main/java/com/blanktheevil/inkmangareader/ui/components/RowLink.kt
@@ -1,29 +1,52 @@
 package com.blanktheevil.inkmangareader.ui.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.blanktheevil.inkmangareader.R
+import com.blanktheevil.inkmangareader.ui.DefaultPreview
 import com.blanktheevil.inkmangareader.ui.InkIcon
 
 @Composable
 fun RowLink(
     title: String,
     @DrawableRes iconResId: Int = R.drawable.round_arrow_forward_24,
+    onClick: () -> Unit = {},
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .clickable(
+                onClick = onClick,
+            )
+        ,
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(text = title)
         InkIcon(resId = iconResId)
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun Preview() = DefaultPreview {
+    Box(modifier = Modifier.fillMaxSize()) {
+        RowLink(title = "Row Link")
     }
 }
