dda08c0d0536622caa008f5ef00aebc27752c3f7
Grayson Gullion <xinflux@gmail.com>
2025-02-25 11:15:16 -0500

    Fix: Transitions not taking effect on all screens.

--- app/src/main/java/com/blanktheevil/inkmangareader/navigation/PrimaryNavGraph.kt
+++ app/src/main/java/com/blanktheevil/inkmangareader/navigation/PrimaryNavGraph.kt
@@ -85,6 +85,8 @@ private fun NavGraphBuilder.simpleComposable(
         arguments = arguments,
         enterTransition = Transitions.slideIn,
         exitTransition = Transitions.slideOut,
+        popEnterTransition = Transitions.slideInRev,
+        popExitTransition = Transitions.slideOutRev,
         content = content,
     )
 }
